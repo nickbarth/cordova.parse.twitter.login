709ed1bc23f461737ff2d31a7f54a90493c97070
Nick Barth <nickbarth@live.com>
2014-07-22 16:53:46 -0600

    Update iOSTwitterLogin.m

--- src/ios/iOSTwitterLogin.m
+++ src/ios/iOSTwitterLogin.m
@@ -7,6 +7,17 @@
 - (void)iOSTwitterLogin:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
 
+    PFUser *currentUser = [PFUser currentUser];
+    
+    if (![PFTwitterUtils isLinkedWithUser:currentUser]) {
+        [PFTwitterUtils linkUser:currentUser block:^(BOOL succeeded, NSError *error) {
+            NSString *result = [NSString stringWithFormat: @"{ \"error\": \"%@\" }", error];
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+            [self writeJavascript:javaScript];
+        }];
+    }
+    
     [PFTwitterUtils logInWithBlock:^(PFUser *user, NSError *error) {
         if (user) {
             NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"sessionToken\": \"%@\" }", user.sessionToken];
