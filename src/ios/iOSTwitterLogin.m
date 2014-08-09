b59778f187fb9bf64aa2ff2cc5adcc7cb319e498
Nick Barth <nickbarth@live.com>
2014-08-09 17:31:38 -0600

    Update iOSTwitterLogin.m

--- src/ios/iOSTwitterLogin.m
+++ src/ios/iOSTwitterLogin.m
@@ -6,36 +6,34 @@
 
 - (void)iOSTwitterLogin:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
-
+    
     PFUser *currentUser = [PFUser currentUser];
     
     if (![PFTwitterUtils isLinkedWithUser:currentUser]) {
         [PFTwitterUtils linkUser:currentUser block:^(BOOL succeeded, NSError *error) {
-            NSString *result = [NSString stringWithFormat: @"{ \"error\": \"%@\" }", error];
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-            [self writeJavascript:javaScript];
+            if (error || !succeeded) {
+                NSString *result = @"{ \"error\": \"Twitter access was not granted.\" }";
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                [self writeJavascript:javaScript];
+            } else {
+                [PFTwitterUtils logInWithBlock:^(PFUser *user, NSError *error) {
+                    if (user) {
+                        NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"sessionToken\": \"%@\" }", user.sessionToken];
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                        NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                        [self writeJavascript:javaScript];
+                    } else {
+                        NSString *result = @"{ \"error\": \"Request for Twitter Login was declined.\" }";
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                        NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                        [self writeJavascript:javaScript];
+                    }
+                }];
+            }
         }];
     }
     
-    [PFTwitterUtils logInWithBlock:^(PFUser *user, NSError *error) {
-        if (user) {
-            NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"sessionToken\": \"%@\" }", user.sessionToken];
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-            [self writeJavascript:javaScript];
-        } else if (error) {
-            NSString *result = [NSString stringWithFormat: @"{ \"error\": \"%@\" }", error];
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-            [self writeJavascript:javaScript];
-        } else {
-            NSString *result = @"{ \"error\": \"Request for Twitter Login was declined.\" }";
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-            [self writeJavascript:javaScript];
-        }
-    }];
 }
 
 @end
